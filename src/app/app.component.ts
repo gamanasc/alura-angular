de34c4e00c6e25fcb74e1675e163609a5d5d8cc8
Lucas Gama <lucasgdon@gmail.com>
2022-02-09 01:02:32 -0300

    Utilização da diretiva *ngFor para rodar o array photos criado no componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  photos = [
+    {
+      url: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png',
+      description: 'Google'
+    },
+    {
+      url: 'https://play-lh.googleusercontent.com/KwUBNPbMTk9jDXYS2AeX3illtVRTkrKVh5xR1Mg4WHd0CG2tV4mrh1z3kXi5z_warlk',
+      description: 'Chrome'
+    },
+    {
+      url: 'https://play-lh.googleusercontent.com/KwUBNPbMTk9jDXYS2AeX3illtVRTkrKVh5xR1Mg4WHd0CG2tV4mrh1z3kXi5z_warlk',
+      description: 'Chrome'
+    },
+    {
+      url: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png',
+      description: 'Google'
+    }
+  ]
+
 }
